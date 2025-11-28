4122c695096a2f7dedbeb072947301b162f704ac
Alaxay8 <alaxay8@gmail.com>
2025-11-28 20:37:52 +0000

    commit

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello, from Python!</h1>"
+    return "<h1>Hello, from Python!</h1>. I'm Alex."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
